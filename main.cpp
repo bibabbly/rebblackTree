8c1c3311062c9aeba6b72e7561305bcd5702d983
bibabbly <bizimungu2004@gmail.com>
2022-02-11 21:40:37 +0200

    never fool with big Boss

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 
 using namespace std;
 
+
 int main()
 {
     cout << "Hello world!" << endl;
     cout << "Preparation of data strucutre prototype " << endl;
+     cout << "I am going to open c** " << endl;
     return 0;
 }
+
+
