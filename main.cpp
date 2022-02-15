052129d4f9d6d2320847df36ed2d3c674dd1fbf1
bibabbly <bizimungu2004@gmail.com>
2022-02-15 17:54:53 +0200

    After learning linux command

--- main.cpp
+++ main.cpp
@@ -8,6 +8,9 @@ int main()
     cout << "Hello world!" << endl;
     cout << "Preparation of data strucutre prototype " << endl;
      cout << "I am going to open c** " << endl;
+         cout << "Hello world!" << endl;
+    cout << "sudo apt-get update" << endl;
+    cout << "sudo apt-get install gedit" << endl;
     return 0;
 }
 
